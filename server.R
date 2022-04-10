6c3ac2acc8937def753019b3cf2cae46c40d3623
David Buch <buch@Davids-MacBook-Air.local>
2022-04-10 12:43:53 -0400

    Adding image files needed for instructions

--- server.R
+++ server.R
@@ -9,42 +9,131 @@
 
 library(shiny)
 
-# Define server logic required to draw a histogram
+loadMetaFile <- function(inFile){
+  if (is.null(inFile))
+    return(NULL)
+  
+  read.csv(inFile$datapath)
+}
+
+loadOdFile <- function(inFile){
+  if (is.null(inFile))
+    return(NULL)
+  
+  read.csv(inFile$datapath, row.names = 1)
+}
+
+createStanData <- function(od, meta, model_params){
+  L <- 1 # number of priors
+  K <- nlevels(factor(meta[,1])) # number of functions
+  priors <- rep(1,K) # prior assignment for each functional coefficient
+  maxExpectedCross <- 100
+  minExpectedCross <- 0.01
+  
+  ls_min <- 1/(pi*maxExpectedCross)
+  ls_max <- 1/(pi*minExpectedCross)
+  
+  x <- as.numeric(row.names(od))
+  y <- as.matrix(od)
+  dm <- model.matrix(~factor(meta[,1]) - 1)
+  
+  stan_data <- list(
+    N = length(x), # number of timepoints
+    P = ncol(y), # number of observations
+    design = dm, # design matrix
+    x = x, # observation times
+    y = t(y), # each row is one well's time series of OD measurements
+    K = K,
+    L = L,
+    prior = rep(1,K), # prior assignment for each functional coefficient
+    alpha_prior = matrix(1, nrow = L, ncol = 2),
+    lengthscale_prior = matrix(1, nrow = L, ncol = 2),
+    sigma_prior = c(1,1),
+    ls_min = 1/(pi*100), # equiv. to "maxExpectedCross = 100"
+    ls_max = 1/(pi*0.01) # equiv. to "minExpectedCross = 0.01"
+  )
+  return(stan_data)
+}
+
 shinyServer(function(input, output) {
-  output$MetaDataFrame <- renderTable({
-    inFile <- input$MetaData
-    if (is.null(inFile))
-      return(NULL)
-    else if (input$MetaIsExcel)
-      return(readxl::read_excel(inFile$datapath))
-    else
-      return(read.csv(inFile$datapath))
+  od <- eventReactive(input$OdData,{
+    loadOdFile(input$OdData)
   })
-  output$OdDataFrame <- renderTable({
-    inFile <- input$OdData
-    if (is.null(inFile))
-      return(NULL)
-    else if (input$OdIsExcel)
-      return(readxl::read_excel(inFile$datapath))
-    else
-      return(read.csv(inFile$datapath))
+  meta <- eventReactive(input$MetaData,{
+    loadMetaFile(input$MetaData)
+  })
+
+  output$OdDataPlot <- renderPlot({
+    req(od())
+    od <- od()
+    matplot(as.numeric(row.names(od)), od, 
+            type = "l", 
+            xlab = "Time", 
+            ylab = "Optical Density",
+            main = "Observed Data")
+  })
+  output$MetaDataFrame <- renderTable({
+    meta()
   })
   
-  sfit <- eventReactive(input$run, {
-    
-    rstan::sampling(
-      object = PhenomStanModel:::stanmodels$phenom_deriv,
-      data = list(),
-      chains = 2,
-      iter = 1000
+  output$FitModelButton <- renderUI({
+    req(od())
+    req(meta())
+    actionButton("run", "Fit Model")
+  })
+  
+  res <- eventReactive(input$run, {
+    od <- od()
+    meta <- meta()
+    validate(
+      need(ncol(od()) == nrow(meta()), "Columns of Optical Density data must match rows of meta data.")
     )
+    
+    model_params <- NULL
+    sampling_params <- NULL
+    if(is.null(sampling_params)){
+      sampling_params <- list(chains = 2, iter = 1000)
+    }
+    
+    stan_data <- createStanData(od, meta, model_params)
+    
+    # attach sampling_params for stan call
+    shinybusy::show_spinner() # Indicate that we are busy sampling
+    attach(sampling_params)
+    stan_fit <- rstan::sampling(
+      object = PhenomStanModel:::stanmodels$phenom_deriv,
+      data = stan_data,
+      chains = chains,
+      iter = iter
+    )
+    detach(sampling_params)
+    shinybusy::hide_spinner() # Indicate that we are done sampling
+    
+    summary(stan_fit)$summary[,c(4,6,8:10)]
   }, ignoreNULL = TRUE)
   
   output$FitSummary <- renderTable({
-    if (is.null(sfit()))
+    if (is.null(res()))
       return(NULL)
     else
-      return(sfit()$summary)
+      return(res())
   })
+
+  output$DownloadButton <- renderUI({
+    req(res()) # requires result to be non-null before showing button
+    downloadButton('Download',"Download Model Fit Summary")
+  })
+  
+  output$Download <- downloadHandler(
+    filename = function(){paste0("phenom_", format(Sys.time(),"%d_%m_%Y__%H_%M_%S"), ".csv")},
+    content = function(fname){
+      if(is.null(res())){
+        write.csv(NULL,fname)
+      }else{
+        write.csv(res(),fname)
+      }
+    },
+    contentType = "text/csv"
+  )
   
 })
--- ui.R
+++ ui.R
@@ -9,7 +9,9 @@
 
 library(shiny)
 library(rstan)
-# We do not load PhenomStanModel but it must be installed
+library(PhenomStanModel)
+library(shinybusy)
+# We do not load rstan, PhenomStanModel, or shinybusy but they must be installed
 
 # Define UI for application
 shinyUI(fluidPage(
@@ -18,6 +20,7 @@ shinyUI(fluidPage(
   titlePanel("Phenom Microbial Growth Model"),
   
   sidebarLayout(
+    position = "right",
     sidebarPanel(
       fileInput("OdData", "Choose Optical Density Data File",
                 accept = c(
@@ -29,21 +32,31 @@ shinyUI(fluidPage(
                 accept = c(
                 "text/csv",                  
                 "text/comma-separated-values,text/plain",
-                ".csv",
-                "application/vnd.ms-excel",
-                "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-                ".xlsx",
-                ".xls")
+                ".csv")
       ),
       tags$hr(),
-      checkboxInput("OdIsExcel", "OD Data is Excel File", FALSE),
-      checkboxInput("MetaIsExcel", "Meta Data is Excel File", TRUE),
-      tags$hr(),
-      actionButton("run", "Fit Model")
+      uiOutput("FitModelButton"),
+      shinybusy::use_busy_spinner(spin = "fading-circle"),
+      br(),
+      uiOutput("DownloadButton")
     ),
     mainPanel(
+      h2("Instructions"),
+      p("Welcome to the beta test of the Phenom web app. This application will analyze microbial growth data collected by an optical density scanner, fitting the flexible *Phenom* model described in Tonner et al. (2020). This preliminary version of the app will only fit the so-called 'MNULL' version of the model outlined in that article. Furthermore, to use this application you will need to make sure you have correctly formatted your data files in advance, as outlined below:"),
+      h3("Optical Density Data"),
+      p("Here we need a 'csv' file with P+1 rows and N+1 columns, where P is the number of time points and N is the number of wells in which optical density measurements were taken. The first row will consist of column labels, and the first column will contain the times at which observations were recorded. The remaining PxN cells will contain optical density measurements. Note that, while the first row must be reserved for names, we need not specify a name for every column. A screenshot of a well-formatted file is provided below."),
+      br(),
+      fluidRow(img(src='od_screenshot.png', width = "40%", align = "left")),
+      br(),
+      h3("Meta Data"),
+      p("Here we need a 'csv' file with N+1 rows and 1 column, where N is the same as above. The first row will consist of column labels, and each row thereafter will correspond to the N wells, in the same order as the columns of the optical density dataset. The column will contain the treatment value of each well. A screenshot of a well-formatted file is provided below."),
+      br(),
+      fluidRow(img(src='meta_screenshot.png', width = "20%", align = "left")),
+      br(),
+      h3("Fitting the Model and Dowloading Results"),
+    p("Once you have uploaded both (1) a file containing a sequence of optical density measurements for a collection of wells , and (2) a meta data file indicating the treatments applied to each well, a button will appear to fit the model. At the same time, a plot of the optical density data and a printout of the metadata will appear below this text so that the user can appraise whether their data have been uploaded and parsed correctly. When the botton is clicked, the app will fit the Phenom model to the provided data. Depending on the size of the dataset, this may take quite a while (10 min to several hours). In the meantime, a spinning wheel will appear in the top right corner of the display to indicate the model is being fit. Once the process is complete, another button will appear which will allow you to download a summary of the model fit, along with MCMC convergence diagnostics."),
       tableOutput("FitSummary"),
-      tableOutput("OdDataFrame"),
+      plotOutput("OdDataPlot"),
       tableOutput("MetaDataFrame")
     )
   )
